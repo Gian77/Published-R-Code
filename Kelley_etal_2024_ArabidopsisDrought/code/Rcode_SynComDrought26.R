0057f73ca46e33279e91482003f7e94f47b0a93b
Gian77 <gian.benucci@gmail.com>
2026-04-01 11:35:52 -0400

    updates

---
+++ Kelley_etal_2024_ArabidopsisDrought/code/Rcode_SynComDrought26.R
@@ -0,0 +1,24 @@
+ *********************************************---------------------------------
+# ************ USEARCH ANALYSIS ***************---------------------------------#
+# Manuscript:   The impact of drought on microbiome assembly in Arabidopsis
+# Authors:      ...
+# Affiliation:  1 Department of Plant Soil and Microbial Sciences, Michigan State University, East Lansing MI 48824
+#               2 Great Lakes Bioenergy Research Center, Michigan State University, East Lansing MI 48824
+#               ...
+# Journal:      ...
+# Date:         April 1, 2026
+# *********************************************---------------------------------
+
+# R options --------------------------------------------------------------------
+options(scipen = 9999, pillar.sigfig=6, digits=6, max.print=10000000) 
+#rm(list = ls())
+
+
+
+
+
+
+
+
+
+
